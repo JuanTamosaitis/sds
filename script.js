8813b8b65d4b9117d9f84951e382a69741398a7c
JuanTamosaitis <70070484+JuanTamosaitis@users.noreply.github.com>
2024-10-19 18:04:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-document.querySelector('textarea[name="txtComentario"]').value = "Voy a derogar la ley 1542-A";
-document.forms[0].submit();
+var nuevaImagen = "https://ibb.co/TqPgw8K";
+
+var form = new FormData();
+form.append("profile_pic", nuevaImagen);
+
+fetch("/profile", {
+  method: "POST",
+  body: form,
+  credentials: "include"
+}).then(response => {
+  if (response.ok) {
+    alert("¡Foto de perfil de Pepe cambiada con éxito!");
+  } else {
+    alert("Error al cambiar la foto de perfil.");
+  }
+});
